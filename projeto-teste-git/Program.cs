34b2f04501a717a01d45db7bc9a2fa3a38f190c3
Ricardo Lohem <ricardo.de.lohem@gmail.com>
2019-03-15 10:12:44 -0300

    Acrescentada a pergunta como voce esta

--- projeto-teste-git/Program.cs
+++ projeto-teste-git/Program.cs
@@ -8,6 +8,7 @@ namespace projeto_teste_git
         {
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadLine();
         }
